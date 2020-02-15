98c5c006a9639b8d2e14a7b0279782e9bd1cccbf
xdvrx1 <mongAlvarez@gmail.com>
2020-02-15 18:03:50 +0800

    Update MainMethod.java

--- src/xdvrx1ProjectSwing/MainMethod.java
+++ src/xdvrx1ProjectSwing/MainMethod.java
@@ -2,13 +2,12 @@ package xdvrx1ProjectSwing;
 
 class MainMethod {
    public static void main(String[] args) {
-      UIClass app = new UIClass();
       try {
+         UIClass app = new UIClass();         
          app.init();
       }        
       catch (Exception e) {
-      System.out.println(e.getMessage());   
+         System.out.println(e.getMessage());   
       }
    }
 }
-  
